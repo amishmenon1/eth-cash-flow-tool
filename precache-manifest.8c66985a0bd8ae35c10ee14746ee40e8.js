7d64d73f25e91eed96707701fdc3b8012a129dab
Amish Menon <amishmenon@gmail.com>
2022-06-25 15:58:01 -0500

    Updates

---
+++ precache-manifest.8c66985a0bd8ae35c10ee14746ee40e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ace43a60af0491bfd6b9",
+    "url": "/eth-cash-flow-tool/static/css/main.dc293fb5.chunk.css"
+  },
+  {
+    "revision": "ace43a60af0491bfd6b9",
+    "url": "/eth-cash-flow-tool/static/js/main.ace43a60.chunk.js"
+  },
+  {
+    "revision": "ec22b39c30e95f15418a",
+    "url": "/eth-cash-flow-tool/static/css/1.8bc535d8.chunk.css"
+  },
+  {
+    "revision": "ec22b39c30e95f15418a",
+    "url": "/eth-cash-flow-tool/static/js/1.ec22b39c.chunk.js"
+  },
+  {
+    "revision": "7a99c1493e963c7a9774",
+    "url": "/eth-cash-flow-tool/static/js/runtime~main.7a99c149.js"
+  },
+  {
+    "revision": "27e342e737477a775684fb6582c6342e",
+    "url": "/eth-cash-flow-tool/static/media/magnifying-glass-icon.27e342e7.png"
+  },
+  {
+    "revision": "4eb253c07061c934764504465102e40e",
+    "url": "/eth-cash-flow-tool/static/media/index.4eb253c0.mjs"
+  },
+  {
+    "revision": "e2db283ee82d9b208f2cc3c3cdeaa4f3",
+    "url": "/eth-cash-flow-tool/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/eth-cash-flow-tool/precache-manifest.8c66985a0bd8ae35c10ee14746ee40e8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/eth-cash-flow-tool/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
