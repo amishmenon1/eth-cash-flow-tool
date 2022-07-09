09594794dbfcd0a5a9fd3b53d67f49eb17fff216
Amish Menon <amishmenon@gmail.com>
2022-07-09 18:43:02 -0500

    removed unnecessary test

--- src/components/Disclaimer/Disclaimer.test.js
+++ src/components/Disclaimer/Disclaimer.test.js
@@ -7,8 +7,6 @@ describe("Disclaimer component", () => {
   it("is displayed with correct message", () => {
     const expectedMessage = "TEST_DISCLAIMER";
     render(<Disclaimer message={expectedMessage} />);
-    const disclaimerElement = screen.queryByText("Disclaimer");
-    expect(disclaimerElement).toBeInTheDocument();
     const disclaimerMessageElement = screen.queryByText(expectedMessage);
     expect(disclaimerMessageElement).toBeInTheDocument();
   });
