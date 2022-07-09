dfc00b73c777a0a2c35233530ecb4465ce646f97
Amish Menon <amishmenon@gmail.com>
2022-07-09 18:32:09 -0500

    updated blockinput tests

--- src/components/BlockInput/BlockInput.test.js
+++ src/components/BlockInput/BlockInput.test.js
@@ -1,60 +1,59 @@
 import React from "react";
 import "@testing-library/jest-dom";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import {
+  getStartBlockElement,
+  getEndBlockElement,
+  getFetchButton,
+  simulateBlockInput,
+} from "../../test/testUtils";
 import BlockInput from ".";
 
 let startBlockElement, endBlockElement, fetchButton;
-
 const mockSubmit = jest.fn();
 
-const getStartBlockElement = () =>
-  screen.getByRole("spinbutton", {
-    name: "Start Block",
-  });
-const getEndBlockElement = () =>
-  screen.getByRole("spinbutton", {
-    name: "End Block",
-  });
-const getFetchButton = () =>
-  screen.getByRole("button", { name: "Fetch Data!" });
-
-const simulateBlockInput = (element, inputValue) => {
-  fireEvent.change(element, { target: { value: inputValue } });
-};
-
-beforeEach(() => {
-  render(<BlockInput onSubmit={mockSubmit} />);
-  startBlockElement = getStartBlockElement();
-  endBlockElement = getEndBlockElement();
-  fetchButton = getFetchButton();
-});
+function clearBlockInputs() {
+  simulateBlockInput(startBlockElement, "");
+  simulateBlockInput(endBlockElement, "");
+}
 
 describe("BlockInput component", () => {
+  beforeEach(() => {
+    render(<BlockInput onSubmit={mockSubmit} />);
+    startBlockElement = getStartBlockElement();
+    endBlockElement = getEndBlockElement();
+    fetchButton = getFetchButton();
+  });
+
+  afterEach(() => {
+    clearBlockInputs();
+  });
+
   it("renders correctly", () => {
     expect(startBlockElement).toBeInTheDocument();
     expect(endBlockElement).toBeInTheDocument();
     expect(fetchButton).toBeInTheDocument();
   });
-});
 
-describe("Start Block Input", () => {
-  it("should change when value is entered", () => {
-    const expectedValue = "123456";
-    simulateBlockInput(startBlockElement, expectedValue);
-    expect(startBlockElement.value).toBe(expectedValue);
+  describe("Start Block Input", () => {
+    it("should change when value is entered", () => {
+      const expectedValue = "123456";
+      simulateBlockInput(startBlockElement, expectedValue);
+      expect(startBlockElement.value).toBe(expectedValue);
+    });
   });
-});
 
-describe("End Block Input", () => {
-  it("should change when value is entered", () => {
-    const expectedValue = "123456";
-    simulateBlockInput(endBlockElement, expectedValue);
-    expect(endBlockElement.value).toBe(expectedValue);
+  describe("End Block Input", () => {
+    it("should change when value is entered", () => {
+      const expectedValue = "123456";
+      simulateBlockInput(endBlockElement, expectedValue);
+      expect(endBlockElement.value).toBe(expectedValue);
+    });
   });
-});
 
-describe("Fetch Data button", () => {
-  it("should be disabled by default", () => {
-    expect(fetchButton).toBeDisabled();
+  describe("Fetch Data button", () => {
+    it("should be disabled by default", () => {
+      expect(fetchButton).toBeDisabled();
+    });
   });
 });
